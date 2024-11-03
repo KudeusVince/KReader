0f0d943d3cc902c8eaf99baa243b3eb99dbc5d72
KudeusVince <contact@kudeus.com>
2024-11-03 14:26:58 +0100

    updated

--- Sources/KReader/Model/KReader.swift
+++ Sources/KReader/Model/KReader.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 import FormBuilder
 
-public struct KReader: Equatable, Hashable {
+public struct KReader: Identifiable, Equatable, Hashable {
     
+    public private(set) var id: String
     public private(set) var name: String
+    public private(set) var type: Int
     
-    public init(name: String) {
+    public init(id: String? = nil, name: String, type: Int) {
+        self.id = id ?? UUID().uuidString
         self.name = name
+        self.type = type
     }
 }
